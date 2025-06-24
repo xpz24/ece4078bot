b9bc02f82ba0e7687f5536029d0802be0570d759
Ding Ze Yang <zeyangding96@gmail.com>
2025-06-24 17:24:58 +0800

    Update listen.py

--- listen.py
+++ listen.py
@@ -121,9 +121,9 @@ def pid_control():
     current_right_pwm = 0
     previous_left_target = 0
     previous_right_target = 0
-    RAMP_RATE = 30  # PWM units per second (adjust this value to tune ramp speed)
-    MIN_RAMP_THRESHOLD = 5  # Only ramp if change is greater than this
-    MIN_PWM_THRESHOLD = 20
+    RAMP_RATE = 250  # PWM units per second (adjust this value to tune ramp speed)
+    MIN_RAMP_THRESHOLD = 10  # Only ramp if change is greater than this
+    MIN_PWM_THRESHOLD = 0
     
     while running:
     
