520d134959d11ffda07cbcde00b5f108f5f34741
Ding Ze Yang <zeyangding96@gmail.com>
2025-06-25 19:44:30 +0800

    Update listen.py

--- listen.py
+++ listen.py
@@ -100,28 +100,28 @@ def reset_encoder():
 def set_motors(left, right):
     global prev_movement, current_movement
     # Pre-Start Kick (Motor Priming)
-    if prev_movement == 'stop' and current_movement in ['forward', 'backward']:
-        print('Pre-starting..')
-        if current_movement  == 'forward':
-            GPIO.output(RIGHT_MOTOR_IN1, GPIO.HIGH)
-            GPIO.output(RIGHT_MOTOR_IN2, GPIO.LOW)
-            GPIO.output(LEFT_MOTOR_IN3, GPIO.HIGH)
-            GPIO.output(LEFT_MOTOR_IN4, GPIO.LOW)
-        elif current_movement == 'backward':
-            GPIO.output(RIGHT_MOTOR_IN1, GPIO.LOW)
-            GPIO.output(RIGHT_MOTOR_IN2, GPIO.HIGH)
-            GPIO.output(LEFT_MOTOR_IN3, GPIO.LOW)
-            GPIO.output(LEFT_MOTOR_IN4, GPIO.HIGH)
-        left_motor_pwm.ChangeDutyCycle(100)
-        right_motor_pwm.ChangeDutyCycle(100)
-        time.sleep(0.05)
+    # if prev_movement == 'stop' and current_movement in ['forward', 'backward']:
+        # print('Pre-starting..')
+        # if current_movement  == 'forward':
+            # GPIO.output(RIGHT_MOTOR_IN1, GPIO.HIGH)
+            # GPIO.output(RIGHT_MOTOR_IN2, GPIO.LOW)
+            # GPIO.output(LEFT_MOTOR_IN3, GPIO.HIGH)
+            # GPIO.output(LEFT_MOTOR_IN4, GPIO.LOW)
+        # elif current_movement == 'backward':
+            # GPIO.output(RIGHT_MOTOR_IN1, GPIO.LOW)
+            # GPIO.output(RIGHT_MOTOR_IN2, GPIO.HIGH)
+            # GPIO.output(LEFT_MOTOR_IN3, GPIO.LOW)
+            # GPIO.output(LEFT_MOTOR_IN4, GPIO.HIGH)
+        # left_motor_pwm.ChangeDutyCycle(100)
+        # right_motor_pwm.ChangeDutyCycle(100)
+        # time.sleep(0.05)
 
     # when pwm is 0, implement Active Braking, better than putting duty cycle to 0 which may cause uneven stopping
     if right > 0:
         pass
         # GPIO.output(RIGHT_MOTOR_IN1, GPIO.HIGH)
         # GPIO.output(RIGHT_MOTOR_IN2, GPIO.LOW)
-        # right_motor_pwm.ChangeDutyCycle(min(right, 100))
+        right_motor_pwm.ChangeDutyCycle(min(right, 100))
     elif right < 0:
         GPIO.output(RIGHT_MOTOR_IN1, GPIO.LOW)
         GPIO.output(RIGHT_MOTOR_IN2, GPIO.HIGH)
@@ -135,7 +135,7 @@ def set_motors(left, right):
         pass
         # GPIO.output(LEFT_MOTOR_IN3, GPIO.HIGH)
         # GPIO.output(LEFT_MOTOR_IN4, GPIO.LOW)
-        # left_motor_pwm.ChangeDutyCycle(min(left, 100))
+        left_motor_pwm.ChangeDutyCycle(min(left, 100))
     elif left < 0:
         GPIO.output(LEFT_MOTOR_IN3, GPIO.LOW)
         GPIO.output(LEFT_MOTOR_IN4, GPIO.HIGH)
