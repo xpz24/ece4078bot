0b09bd0a2a1595be027e9fb6b65f140389ed5cdb
Ding Ze Yang <zeyangding96@gmail.com>
2025-06-26 18:19:13 +0800

    Update listen.py

--- listen.py
+++ listen.py
@@ -208,8 +208,6 @@ def pid_control():
     previous_right_target = 0
     
     while running:
-        if GPIO.input(LEFT_ENCODER) or GPIO.input(RIGHT_ENCODER):
-            print('hi')
     
         current_time = monotonic()
         dt = current_time - last_time
