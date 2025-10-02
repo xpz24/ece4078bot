cdd94db1c274aabcb0e85bf275146f28db660d63
xpz24 <shahum24@gmail.com>
2025-10-03 01:21:08 +0800

    velocity measurement serverside

--- listen.py
+++ listen.py
@@ -3,7 +3,9 @@ import struct
 import io
 import threading
 import time
+import math
 from time import monotonic
+from collections import deque
 import RPi.GPIO as GPIO  # type: ignore
 from picamera2 import Picamera2  # type: ignore
 
@@ -30,10 +32,20 @@ MAX_CORRECTION = 80  # Maximum PWM correction value
 MAX_INTEGRAL = MAX_CORRECTION
 
 # Global variables
+WINDOW_SIZE = 5
 running = True
 left_pwm, right_pwm = 0, 0
 left_count, right_count = 0, 0
-left_w, right_w = 0.0, 0.0
+omegaL_f, omegaR_f = 0.0, 0.0
+omegaL_q = deque(maxlen=WINDOW_SIZE)
+omegaR_q = deque(maxlen=WINDOW_SIZE)
+vL_f, vR_f = 0.0, 0.0
+Wq = deque(maxlen=WINDOW_SIZE)
+Vq = deque(maxlen=WINDOW_SIZE)
+W, V = 0.0, 0.0
+last_L = 0
+last_R = 0
+last_time = None
 prev_left_state, prev_right_state = None, None
 use_ramping = True
 RAMP_RATE_ACC = 180  # PWM units per second (adjust this value to tune ramp speed)
@@ -42,6 +54,11 @@ RIGHT_WHEEL_OFFSET = 1  # 5% boost for weaker wheel
 MIN_RAMP_THRESHOLD = 30  # Only ramp if change is greater than this
 MIN_PWM_THRESHOLD = 30
 current_movement, prev_movement = "stop", "stop"
+
+# locks
+encoder_lock = threading.Lock()
+pwm_lock = threading.Lock()
+movement_lock = threading.Lock()
 
 
 def clamp(x: int, minimum: int, maximum: int):
@@ -86,7 +103,8 @@ def left_encoder_callback(channel):
     # Check for actual state change. Without this, false positive happens due to electrical noise
     # After testing, debouncing not needed
     if prev_left_state is not None and current_state != prev_left_state:
-        left_count += 1
+        with encoder_lock:
+            left_count += 1
         prev_left_state = current_state
 
     elif prev_left_state is None:
@@ -99,7 +117,8 @@ def right_encoder_callback(channel):
     current_state = GPIO.input(RIGHT_ENCODER)
 
     if prev_right_state is not None and current_state != prev_right_state:
-        right_count += 1
+        with encoder_lock:
+            right_count += 1
         prev_right_state = current_state
 
     elif prev_right_state is None:
@@ -108,20 +127,24 @@ def right_encoder_callback(channel):
 
 def reset_encoder():
     global left_count, right_count
-    left_count, right_count = 0, 0
+    with encoder_lock:
+        left_count, right_count = 0, 0
 
 
 def set_motors(left, right):
     global prev_movement, current_movement
+    with movement_lock:
+        p_movement = prev_movement
+        c_movement = current_movement
 
     # Pre-Start Kick (Motor Priming), to reduce initial jerk and slight orientation change
-    if prev_movement == "stop" and current_movement in ["forward", "backward"]:
-        if current_movement == "forward":
+    if p_movement == "stop" and c_movement in ["forward", "backward"]:
+        if c_movement == "forward":
             GPIO.output(RIGHT_MOTOR_IN1, GPIO.HIGH)
             GPIO.output(RIGHT_MOTOR_IN2, GPIO.LOW)
             GPIO.output(LEFT_MOTOR_IN3, GPIO.HIGH)
             GPIO.output(LEFT_MOTOR_IN4, GPIO.LOW)
-        elif current_movement == "backward":
+        elif c_movement == "backward":
             GPIO.output(RIGHT_MOTOR_IN1, GPIO.LOW)
             GPIO.output(RIGHT_MOTOR_IN2, GPIO.HIGH)
             GPIO.output(LEFT_MOTOR_IN3, GPIO.LOW)
@@ -170,10 +193,13 @@ def apply_min_threshold(pwm_value, min_threshold):
 
 def pid_control():
     # Only applies for forward/backward, not turning
-    global left_pwm, right_pwm, left_count, right_count, use_PID, KP, KI, KD, rKP, rKI, rKD, prev_movement, current_movement, left_w, right_w
+    global left_pwm, right_pwm, use_PID, KP, KI, KD, rKP, rKI, rKD, prev_movement, current_movement
+    with pwm_lock:
+        l_pwm = left_pwm
+        r_pwm = right_pwm
 
-    integral_rotation_left = 0.0  # Double check if it resets here
-    integral_rotation_right = 0.0  # Double check if it resets here
+    integral_rotation_left = 0.0
+    integral_rotation_right = 0.0
     integral_linear_forward = 0.0
     integral_linear_back = 0.0
     last_error_rotation = 0.0
@@ -181,7 +207,7 @@ def pid_control():
     last_time = monotonic()
     last_derivative_linear = 0.0
     last_derivative_rotation = 0.0
-    alpha = 0.7
+    alpha = 0.1
     switch_mode = [False, False]  # [left, right]
 
     # Ramping variables & params
@@ -194,13 +220,14 @@ def pid_control():
         last_time = current_time
 
         prev_movement = current_movement
-        if left_pwm > 0 and right_pwm > 0:
+
+        if l_pwm > 0 and r_pwm > 0:
             requested_movement = "forward"
-        elif left_pwm < 0 and right_pwm < 0:
+        elif l_pwm < 0 and r_pwm < 0:
             requested_movement = "backward"
-        elif left_pwm == 0 and right_pwm == 0:
+        elif l_pwm == 0 and r_pwm == 0:
             requested_movement = "stop"
-        elif left_pwm < 0 and right_pwm > 0:
+        elif l_pwm < 0 and r_pwm > 0:
             requested_movement = "rotate_left"
         else:
             requested_movement = "rotate_right"
@@ -212,8 +239,8 @@ def pid_control():
             current_movement = prev_movement
 
         if not use_PID:
-            target_left_pwm = left_pwm
-            target_right_pwm = right_pwm
+            target_left_pwm = l_pwm
+            target_right_pwm = r_pwm
         else:
             if current_movement in [
                 "forward",
@@ -223,7 +250,8 @@ def pid_control():
             ]:
 
                 if current_movement in ["forward", "backward"]:
-                    error = left_w - right_w
+                    with encoder_lock:
+                        error = omegaL_f - omegaR_f
                     # print(f"linear! leftV {left_v}, rightV{right_v}")
                     derivative = KD * (error - last_error_linear) / dt if dt > 0 else 0
                     derivative = (
@@ -252,7 +280,7 @@ def pid_control():
                         proportional + I + derivative, -MAX_CORRECTION, MAX_CORRECTION
                     )
                 else:
-                    error = left_w + right_w
+                    error = omegaL_f + omegaR_f
                     # print(f"Rotation! leftV {left_v}, rightV{right_v}")
                     derivative = (
                         rKD * (error - last_error_rotation) / dt if dt > 0 else 0
@@ -284,20 +312,22 @@ def pid_control():
                     )
 
                 if current_movement in ["forward", "backward"]:
-                    target_left_pwm = left_pwm - correction
-                    target_right_pwm = right_pwm + correction
+                    target_left_pwm = l_pwm - correction
+                    target_right_pwm = r_pwm + correction
                 elif current_movement in ["rotate_left", "rotate_right"]:
-                    target_left_pwm = left_pwm - correction
-                    target_right_pwm = right_pwm - correction
+                    target_left_pwm = l_pwm - correction
+                    target_right_pwm = r_pwm - correction
             else:
                 # Reset when stopped
-                # integral_linear = 0
-                # integral_rotation = 0
+                # integral_linear_forward = 0
+                # integral_linear_back = 0
+                # integral_rotation_right = 0
+                # integral_rotation_left = 0
                 last_error_linear = 0
                 last_error_rotation = 0
                 reset_encoder()
-                target_left_pwm = left_pwm
-                target_right_pwm = right_pwm
+                target_left_pwm = l_pwm
+                target_right_pwm = r_pwm
                 # print(f"Stopped! leftV {left_v}, rightV{right_v}")
 
         # (Optional for debugging symmetry: disable offset first)
@@ -341,9 +371,10 @@ def pid_control():
         # if ramp_left_pwm != 0: # print for debugging purpose
         #     print(f"(Left PWM, Right PWM)=({ramp_left_pwm:.2f},{ramp_right_pwm:.2f}), (Left Enc, Right Enc)=({left_count}, {right_count})")
 
-        print(
-            f"left_count={left_count}, right_count={right_count}, diff={left_count-right_count}, correction={correction if use_PID else 0}"
-        )
+        # with encoder_lock:
+        #     print(
+        #         f"left_count={left_count}, right_count={right_count}, diff={left_count-right_count}, correction={correction if use_PID else 0}"
+        #     )
         time.sleep(0.01)
 
 
@@ -410,7 +441,7 @@ def pid_config_server():
             print(f"PID config client connected")
 
             try:
-                # Receive PID constants (4 floats)
+                # Receive PID constants (7 floats)
                 data = client_socket.recv(28)
                 if data and len(data) == 28:
                     use_PID, KP, KI, KD, rKP, rKI, rKD = struct.unpack("!fffffff", data)
@@ -446,7 +477,7 @@ def pid_config_server():
 
 
 def wheel_server():
-    global left_pwm, right_pwm, running, left_count, right_count, left_w, right_w
+    global left_pwm, right_pwm, running, vL_f, vR_f, V, W
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -462,24 +493,25 @@ def wheel_server():
             while running:
                 try:
                     # Receive speed (4 bytes for each value)
-                    data = client_socket.recv(16)
-                    if not data or len(data) != 16:
+                    data = client_socket.recv(8)
+                    if not data or len(data) != 8:
                         print("Wheel client sending speed error")
                         break
 
                     # Unpack speed values and convert to PWM
-                    left_speed, right_speed, left_w, right_w = struct.unpack(
-                        "!ffff", data
-                    )
+                    left_speed, right_speed = struct.unpack("!ff", data)
                     # print(f"Received wheel: left_speed={left_speed:.4f}, right_speed={right_speed:.4f}")
-                    left_pwm, right_pwm = (
-                        left_speed * 100,
-                        right_speed * 100 * RIGHT_WHEEL_OFFSET,
-                    )
+                    with pwm_lock:
+                        left_pwm, right_pwm = (
+                            left_speed * 100,
+                            right_speed * 100 * RIGHT_WHEEL_OFFSET,
+                        )
 
-                    # Send encoder counts back
-                    response = struct.pack("!ii", left_count, right_count)
+                    # Send vL_f, vR_f, V, W back
+                    with encoder_lock:
+                        response = struct.pack("!ffff", vL_f, vR_f, V, W)
                     client_socket.sendall(response)
+                    time.sleep(0.001)
 
                 except Exception as e:
                     print(f"Wheel client disconnected")
@@ -492,6 +524,88 @@ def wheel_server():
             client_socket.close()
 
     server_socket.close()
+
+
+def measure_velocities():
+    global last_L, last_R, last_time, omegaL_f, omegaR_f, vL_f, vR_f, V, W
+
+    ticks_per_rev = 20
+    r = 0.033
+    alpha = 0.1  # tau = T/alpha -> 0.005/0.1 = 50ms
+    max_omega = 2  # Big jump protection
+    baseline = 0.115
+    time2stop = 0.5
+
+    while running:
+        with pwm_lock:
+            sL = left_pwm / abs(left_pwm) if left_pwm != 0 else 0
+            sR = right_pwm / abs(right_pwm) if right_pwm != 0 else 0
+
+        with encoder_lock:
+            L = left_count
+            R = right_count
+
+        now = time.monotonic()
+        if last_L is None:
+            last_L = L
+            last_R = R
+            last_time = now
+            return
+
+        dt = now - last_time
+        last_time = now
+        dL = L - last_L
+        dR = R - last_R
+        last_L = L
+        last_R = R
+
+        if dL != 0:
+            omegaL = sL * 2 * math.pi * (dL / ticks_per_rev) / dt
+            last_tick_L = now
+        else:
+            t_since = now - last_tick_L
+            if t_since < time2stop:
+                omegaL = sL * 2 * math.pi / (ticks_per_rev * t_since)
+            else:
+                omegaL = 0.0
+
+        if dR != 0:
+            omegaR = sR * 2 * math.pi * (dR / ticks_per_rev) / dt
+            last_tick_R = now
+        else:
+            t_since = now - last_tick_R
+            if t_since < time2stop:
+                omegaR = sR * 2 * math.pi / (ticks_per_rev * t_since)
+            else:
+                omegaR = 0.0
+
+        # This is ok since big jump can only occur for one cycle
+        if abs(omegaL) > max_omega:
+            omegaL = 0.0
+        if abs(omegaR) > max_omega:
+            omegaR = 0.0
+
+        vL = omegaL * r
+        vR = omegaR * r
+        # omegaL_q.append(omegaL)
+        # omegaR_q.append(omegaR)
+
+        Vq.append((vL + vR) / 2)
+        Wq.append((vR - vL) / baseline)
+
+        with encoder_lock:
+            # omegaL_f = sum(omegaL_q)/len(omegaL_q) if len(omegaL_q) > 0 else 0.0
+            # omegaR_f = sum(omegaR_q)/len(omegaR_q) if len(omegaR_q) > 0 else 0.0
+            # V = sum(Vq)/len(Vq) if len(Vq) > 0 else 0.0
+            # W = sum(Wq)/len(Wq) if len(Wq) > 0 else 0.0
+            V = (1 - alpha) * V + alpha * ((vL + vR) / 2)
+            W = (1 - alpha) * W + alpha * ((vR - vL) / baseline)
+            omegaL_f = (1 - alpha) * omegaL_f + alpha * omegaL
+            omegaR_f = (1 - alpha) * omegaR_f + alpha * omegaR
+            vL_f = omegaL_f * r
+            vR_f = omegaR_f * r
+
+        time.sleep(0.005)
 
 
 def main():
@@ -512,6 +626,11 @@ def main():
         pid_config_thread = threading.Thread(target=pid_config_server)
         pid_config_thread.daemon = True
         pid_config_thread.start()
+
+        # Start velocity measurement thread
+        velocity_thread = threading.Thread(target=measure_velocities)
+        velocity_thread.daemon = True
+        velocity_thread.start()
 
         # Start wheel server (main thread)
         wheel_server()
