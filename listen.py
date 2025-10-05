1857962a2b5c1d999cb6f8f7557d0dc14df66033
xpz24 <shahum24@gmail.com>
2025-10-05 14:01:29 +0800

    use dt from server side

--- listen.py
+++ listen.py
@@ -46,6 +46,7 @@ vL_f, vR_f = 0.0, 0.0
 Wq = deque(maxlen=WINDOW_SIZE)
 Vq = deque(maxlen=WINDOW_SIZE)
 W, V = 0.0, 0.0
+dt = 0.0
 last_L = 0
 last_R = 0
 last_time = None
@@ -556,7 +557,7 @@ def pid_config_server():
 
 
 def wheel_server():
-    global left_pwm, right_pwm, running, vL_f, vR_f, V, W
+    global left_pwm, right_pwm, running, vL_f, vR_f, V, W, dt
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -590,7 +591,7 @@ def wheel_server():
 
                     # Send vL_f, vR_f, V, W back
                     with encoder_lock:
-                        response = struct.pack("!ffff", vL_f, vR_f, V, W)
+                        response = struct.pack("!fffff", vL_f, vR_f, V, W, dt)
                     client_socket.sendall(response)
                     time.sleep(0.001)
 
@@ -608,7 +609,7 @@ def wheel_server():
 
 
 def measure_velocities():
-    global last_L, last_R, last_time, omegaL_f, omegaR_f, vL_f, vR_f, V, W
+    global last_L, last_R, last_time, omegaL_f, omegaR_f, vL_f, vR_f, V, W, dt
 
     ticks_per_rev = 20
     r = 0.033
@@ -637,15 +638,17 @@ def measure_velocities():
             last_time = now
             continue
 
-        dt = now - last_time
+        with encoder_lock:
+            dt = now - last_time
+        dt_loop = now - last_time
         last_time = now
         dL = L - last_L
         dR = R - last_R
         last_L = L
         last_R = R
 
-        omegaL = sL * 2 * math.pi * (dL / ticks_per_rev) / dt
-        omegaR = sR * 2 * math.pi * (dR / ticks_per_rev) / dt
+        omegaL = sL * 2 * math.pi * (dL / ticks_per_rev) / dt_loop
+        omegaR = sR * 2 * math.pi * (dR / ticks_per_rev) / dt_loop
         # This is ok since big jump can only occur for one cycle
         if abs(omegaL) > max_omega:
             omegaL = 0.0
